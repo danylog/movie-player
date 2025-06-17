d41f59cb94f15ac42dce8b49f8477f00add9f6b7
nevnev19 <neva.nur@icloud.com>
2025-06-18 00:42:09 +0200

    Update main.py
    farbe geändert

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ font = pygame.font.Font(None, 42)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 YELLOW = (255, 255, 0)
-PURPLE = (102, 0, 100) 
+PURPLE = (102, 100, 100) 
 
 # --- Filme laden ---
 VIDEO_FOLDER = "/home/pi/tvprojekt/videos"
