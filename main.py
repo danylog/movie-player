76e217c775e4c1f99891ca529926a02dfc0f3f89
nevnev19 <neva.nur@icloud.com>
2025-07-01 14:49:26 +0200

    Update main.py
    visuelles feedback

--- main.py
+++ main.py
@@ -140,6 +140,15 @@ while running:
 
 
                         fix_runtime_dir_permissions()  # Fix permissions before playing video
+
+# VISUELLES FEEDBACK HINZUFÜGEN
+            text = font.render(f"{i+1}. {filename}", True, YELLOW)
+            pygame.draw.rect(screen, BLACK, (tx, ty, WIDTH-120, 40))  # Clear previous text
+            screen.blit(text, (tx, ty))
+            pygame.display.flip()
+            pygame.event.pump()
+            time.sleep(0.3)  # 300ms warten, damit man’s sieht
+                        
                         still_running = play_video(filepath)
                         if not still_running:
                             running = False
